03cf0c701161432f7e1d259c445129ebef0b6edf
romualdascoder <72582679+romualdascoder@users.noreply.github.com>
2021-01-16 16:18:53 +0200

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,23 @@
+const section = document.querySelector(".nav-list");
+let currentPixel = window.pageYOffset;
+
+//looper keeps running and keeps track of where the new pixel is
+
+const looper = function () {
+  const newPixel = window.pageYOffset;
+  const diff = newPixel - currentPixel
+  const speed = diff * 0.35;
+  
+  section.style.transform = "skewY(" + speed + "deg)"
+  
+  
+  currentPixel = newPixel;
+  
+  requestAnimationFrame(looper)
+}
+
+looper();
+
+
+            
+
