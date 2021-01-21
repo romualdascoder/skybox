bb794260dccc194126182089e7078626c5da7d21
romualdascoder <72582679+romualdascoder@users.noreply.github.com>
2021-01-21 21:26:01 +0200

    Add files via upload

--- scroll.js
+++ scroll.js
@@ -1,20 +1,23 @@
 const section = document.querySelector(".nav-list");
 const aboutImg = document.querySelector(".about-img");
-
 const projectsTitle = document.querySelector(".projects-title");
+const skillsTitle = document.querySelector(".skills-title");
+const skillsGallery = document.querySelector(".skills-gallery");
+
 let currentPixel = window.pageYOffset;
 
-//looper keeps running and keeps track of where the new pixel is
+//looper keeps running and track of where the new pixel is
 
 const looper = function () {
   const newPixel = window.pageYOffset;
   const diff = newPixel - currentPixel
-  const speed = diff * 0.35;
+  const speed = diff * 0.20;
   
   section.style.transform = "skewY(" + speed + "deg)"
   aboutImg.style.transform = "skewY(" + speed + "deg)"
-
-    projectsTitle.style.transform = "skewY(" + speed + "deg)"
+  projectsTitle.style.transform = "skewY(" + speed + "deg)"
+  skillsTitle.style.transform = "skewY(" + speed + "deg)"
+  skillsGallery.style.transform = "skewY(" + speed + "deg)"
   
   currentPixel = newPixel;
   
