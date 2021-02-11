55f1a8affb4d5acb95277f553aa215ad1de572b5
romualdascoder <72582679+romualdascoder@users.noreply.github.com>
2021-02-11 16:47:38 +0200

    Add files via upload

---
+++ js/analytics.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-E5G00Q4WQ5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-E5G00Q4WQ5');
+</script>
