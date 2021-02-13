51519fb446cbe2182a974089a3a2647f084742dd
romualdascoder <72582679+romualdascoder@users.noreply.github.com>
2021-02-14 01:42:02 +0200

    Add files via upload

--- js/analytics.js
+++ js/analytics.js
@@ -1,8 +1,17 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-E5G00Q4WQ5"></script>
-<script>
+
+function loadScriptAsync(scriptSrc, callback) {
+  if (typeof callback !== 'function') {
+      throw new Error('Not a valid callback for async script load');
+  }
+  var script = document.createElement('script');
+  script.onload = callback;
+  script.src = scriptSrc;
+  document.head.appendChild(script);
+}
+
+loadScriptAsync('https://www.googletagmanager.com/gtag/js?id=G-E5G00Q4WQ5', function(){
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'G-E5G00Q4WQ5');
-</script>
+})
